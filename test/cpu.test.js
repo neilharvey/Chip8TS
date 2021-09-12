f4741393048fc7420befabbc4c2d7125f51d19a8
Neil Harvey <neil.harvey@hotmail.co.uk>
2021-09-12 23:36:14 +0100

    Remove JS files

--- test/cpu.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cpu_1 = require("../src/cpu");
-test('cpu intialises fields correctly', function () {
-    var cpu = new cpu_1.Cpu();
-    expect(cpu.memory.length).toBe(4096);
-});
