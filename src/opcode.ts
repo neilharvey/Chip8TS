cd9cd9be6648744612b4ccb97d843bbaecb8c7e8
neilharvey <neil.harvey@hotmail.co.uk>
2021-09-21 18:54:26 +0100

    Implement opcode instructions

--- src/opcode.ts
+++ src/opcode.ts
@@ -28,17 +28,55 @@ export class Opcode {
                     return "CLS";
                 } else if (this.value == 0x00EE) {
                     return "RET";
-                } else {
-                    return "";
-                }
+                } 
+
+                break;
             case 0x1000:
                 return `JP ${this.nnn.toHex(3)}`;
+            case 0x2000:
+                return `CALL ${this.nnn.toHex(3)}`;
+            case 0x3000:
+                return `SE V${this.x} ${this.kk.toHex(2)}`;
+            case 0x4000:
+                return `SNE V${this.x} ${this.kk.toHex(2)}`;
+            case 0x5000:
+                return `SE V${this.x} V${this.y}`;
             case 0x6000:
                 return `LD V${this.x}, ${this.kk.toHex(2)}`;
             case 0x7000:
                 return `ADD V${this.x}, ${this.kk.toHex(2)}`;
+            case 0x8000:
+                switch(this.n) {
+                    case 0:
+                        return `LD V${this.x}, V${this.y}`;
+                    case 1:
+                        return `OR V${this.x}, V${this.y}`;
+                    case 2:
+                        return `AND V${this.x}, V${this.y}`;
+                    case 3:
+                        return `XOR V${this.x}, V${this.y}`;
+                    case 4:
+                        return `ADD V${this.x}, V${this.y}`;                    
+                    case 5:
+                        return `SUB V${this.x}, V${this.y}`;
+                    case 6:
+                        return `SHR V${this.x}, V${this.y}`;
+                    case 7:
+                        return `SUBN V${this.x}, V${this.y}`;
+                    case 0xE:
+                        return `SHL V${this.x}, V${this.y}`;
+                    break;
+                }
+
+                break;
+            case 0x9000:
+                return `SNE V${this.x}, V${this.y}`;
             case 0xA000:
                 return `LD I, ${this.nnn.toHex(3)}`;
+            case 0xB000:
+                return `JP V0, ${this.nnn.toHex(3)}`;
+            case 0xC000:
+                return `RND V${this.x} ${this.kk.toHex(2)}`;
             case 0xD000:
                 return `DRW V${this.x}, V${this.y}, ${this.n.toHex(1)}`;
         }
