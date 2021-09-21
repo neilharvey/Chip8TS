2afa7f5f7198f82b38bfeaebf0a9dad449d3d035
neilharvey <neil.harvey@hotmail.co.uk>
2021-09-21 16:39:11 +0100

    Add test cases for opcode instruction map

--- test/opcode.test.ts
+++ test/opcode.test.ts
@@ -51,26 +51,43 @@ describe('Opcode', () => {
 
     describe('instruction', () => {
 
-        function assertOpcodeInstruction(value:number, instruction:string) {
+        let data = [
+            ['00E0 is CLS', 0x00E0, 'CLS'],
+            ['00EE is RET', 0x00EE, 'RET'],
+            ['1nnn is JP nnn', 0x122A, 'JP 22a'],
+            //['2nnn is CALL nnn', 0x123, 'CALL 123'],
+            // ['3xkk is SE Vx, kk', 0x3123, 'SE V1 23'],
+            // ['4xkk is SNE Vx, kk', 0x4123, 'SNE V1 23'],
+            // ['5xy0 is SE Vx, Vy', 0x5120, 'SE V1 V2'],
+            ['6xkk is LD Vx, kk', 0x600C, 'LD V0, 0c'],
+            ['7xkk is ADD Vx, kk', 0x7009, 'ADD V0, 09'],
+            // ['8xy0 is LD Vx, Vy', 0x8120, 'LD V1, V2'],
+            // ['8xy1 is OR Vx, Vy', 0x8121, 'OR V1, V2'],
+            // ['8xy2 is AND Vx, Vy', 0x8122, 'AND V1, V2'],
+            // ['8xy3 is XOR Vx, Vy', 0x8123, 'XOR V1, V2'],
+            // ['8xy4 is ADD Vx, Vy', 0x8124, 'ADD V1, V2'],
+            // ['8xy5 is SUB Vx, Vy', 0x8125, 'SUB V1, V2'],
+            // ['8xy6 is SHR Vx {, Vy}', 0x8126, 'SHR V1 {, V2}'],
+            // ['8xy7 is SUBN Vx, Vy', 0x8127, 'SUBN V1, V2'],
+            ['Annn is LD I, nnn', 0xA22A, 'LD I, 22a'],
+            ['Dxyn is DRW Vx, Vy, n', 0xD01F, 'DRW V0, V1, f']
+        ];
 
-            var msb = (value & 0xFF00) >> 8;
-            var lsb = value & 0x00FF;
+        data.forEach((item) => {
 
-            var opcode = new Opcode(msb, lsb);
+            let theory:string = <string>item[0];
+            let value:number = <number>item[1];
+            let instruction:string = <string>item[2];
 
-            assert.strictEqual(opcode.instruction(),instruction);
-            
-        }
-
-        // Opcodes
-        it('0x00E0 is CLS', () => assertOpcodeInstruction(0x00E0, 'CLS'));
-        it('0x00EE is RET', () => assertOpcodeInstruction(0x00EE, 'RET'));
-        it('0x1nnn is JP nnn', () => assertOpcodeInstruction(0x122A, 'JP 22a'));
-        it('0x6xkk is LD Vx, kk', () => assertOpcodeInstruction(0x600C, 'LD V0, 0c'));
-        it('0x7xkk is ADD Vx, kk', () => assertOpcodeInstruction(0x7009, 'ADD V0, 09'));
-        it('0xAnnn is LD I, nnn', () => assertOpcodeInstruction(0xA22A, 'LD I, 22a'));
-        it('0xDxyn is DRW Vx, Vy, n', () => assertOpcodeInstruction(0xD01F, 'DRW V0, V1, f'));
-
+            it(theory, () => {
+                var msb = (value & 0xFF00) >> 8;
+                var lsb = value & 0x00FF;
+    
+                var opcode = new Opcode(msb, lsb);
+    
+                assert.strictEqual(opcode.instruction(), instruction);
+            });
+        });
     });
 
 });
